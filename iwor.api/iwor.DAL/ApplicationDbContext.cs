a656127393adf7f215863478695bb7fbaceb5ccf
Andrew <andrew.1.kulikov@gmail.com>
2019-11-13 01:32:58 +0300

    Add User identity to api

--- iwor.api/iwor.DAL/ApplicationDbContext.cs
+++ iwor.api/iwor.DAL/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿using System.Reflection;
+using iwor.DAL.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace iwor.DAL
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
---
+++ iwor.api/iwor.DAL/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace iwor.DAL.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
