11e2b1f6608d79b65451b87fe03fbe1a0336247a
Andrew <andrew.1.kulikov@gmail.com>
2019-11-13 23:01:04 +0300

    Add automigrate

--- iwor.api/iwor.api/Startup.cs
+++ iwor.api/iwor.api/Startup.cs
@@ -62,6 +62,8 @@ namespace iwor.api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            UpdateDatabase(app);
+
             if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
             app.UseHttpsRedirection();
@@ -72,6 +74,16 @@ namespace iwor.api
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope();
+            using var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+
+            context.Database.Migrate();
         }
     }
 }
