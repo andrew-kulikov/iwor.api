6909def86f0b405ce41a03286f32b162c41490db
Andrew <andrew.1.kulikov@gmail.com>
2019-11-17 22:24:22 +0300

    Implement base repository

---
+++ iwor.api/iwor.DAL/Repositories/Repository.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iwor.core.Entities;
+using iwor.core.Repositories;
+using iwor.core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace iwor.DAL.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly ApplicationDbContext Context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await Context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await Context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await Context.Set<T>().AddAsync(entity);
+            await Context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(Context.Set<T>().AsQueryable(), spec);
+        }
+    }
+}
