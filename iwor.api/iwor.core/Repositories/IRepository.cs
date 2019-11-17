6bb03f9fcc458a8dceb9489c42bc225bb781a68c
Andrew <andrew.1.kulikov@gmail.com>
2019-11-17 22:15:22 +0300

    Add specification and repository interfaces

--- iwor.api/iwor.core/Repositories/IRepository.cs
+++ iwor.api/iwor.core/Repositories/IRepository.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using iwor.core.Entities;
+using iwor.core.Specifications;
 
 namespace iwor.core.Repositories
 {
-    public interface IRepository<T>
+    public interface IRepository<T> where T : BaseEntity
     {
-
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<int> CountAsync(ISpecification<T> spec);
     }
 }
---
+++ iwor.api/iwor.core/Specifications/BaseSpecification.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iwor.core.Specifications
+{
+    class BaseSpecification
+    {
+    }
+}
