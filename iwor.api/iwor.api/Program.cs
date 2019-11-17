b53f5fe8cfa47038059ac706ac0d3ba5ab47f51d
Andrew <andrew.1.kulikov@gmail.com>
2019-11-17 20:09:51 +0300

    Remove docker and manually set launch urls

--- iwor.api/iwor.api/Program.cs
+++ iwor.api/iwor.api/Program.cs
@@ -13,7 +13,10 @@ namespace iwor.api
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>().UseUrls("https://localhost:55221", "http://localhost:55220");
+                });
         }
     }
 }
