4342164571b8a8deb6024ce9c852a86bd1502346
Andrew <andrew.1.kulikov@gmail.com>
2019-11-20 03:05:45 +0300

    Add id to auction dto

--- iwor.api/iwor.api/DTOs/AuctionDto.cs
+++ iwor.api/iwor.api/DTOs/AuctionDto.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
-using iwor.core.Entities;
+﻿using System;
+using System.Collections.Generic;
 
 namespace iwor.api.DTOs
 {
     public class AuctionDto : NewAuctionDto
     {
+        public Guid Id { get; set; }
         public string OwnerId { get; set; }
         public string Status { get; set; }
         public ICollection<PriceRaiseDto> PriceRaises { get; set; }
