fafc9015474fb37fea2f4e4074ae3ac85650c4b2
Andrew <andrew.1.kulikov@gmail.com>
2019-11-24 22:25:27 +0300

    Add endpoint for user deletion

--- iwor.api/iwor.api/Controllers/UsersController.cs
+++ iwor.api/iwor.api/Controllers/UsersController.cs
@@ -8,6 +8,7 @@ using iwor.core.Entities;
 using iwor.core.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace iwor.api.Controllers
@@ -19,18 +20,21 @@ namespace iwor.api.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IRepository<ApplicationUser> _repository;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public UsersController(IRepository<ApplicationUser> repository, IMapper mapper)
+        public UsersController(IRepository<ApplicationUser> repository, IMapper mapper,
+            UserManager<ApplicationUser> userManager)
         {
             _repository = repository;
             _mapper = mapper;
+            _userManager = userManager;
         }
 
 
         [HttpGet]
         [Route("")]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ICollection<PublicUserProfile>))]
-        public async Task<IActionResult> GetById()
+        public async Task<IActionResult> GetAll()
         {
             var users = await _repository.ListAllAsync();
             var profiles = _mapper.Map<ICollection<UserProfile>>(users)
@@ -53,6 +57,24 @@ namespace iwor.api.Controllers
             var profile = _mapper.Map<UserProfile>(user) as PublicUserProfile;
 
             return Ok(ResponseDto<PublicUserProfile>.Ok(profile));
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        [Authorize(Roles = "Admin")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var user = await _repository.GetByIdAsync(id);
+
+            if (user == null) return NotFound();
+
+            var result = await _userManager.DeleteAsync(user);
+
+            if (result.Succeeded) return NoContent();
+
+            return StatusCode(400, ResponseDto<int>.BadRequest("Невозможно удалить пользователя"));
         }
     }
 }
