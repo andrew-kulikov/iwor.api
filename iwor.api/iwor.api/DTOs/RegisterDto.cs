9b15145e602f4bda045d3ffd1512ff006669c1a1
Andrew <andrew.1.kulikov@gmail.com>
2019-11-25 21:15:45 +0300

    Add phone and image to registration

--- iwor.api/iwor.api/DTOs/RegisterDto.cs
+++ iwor.api/iwor.api/DTOs/RegisterDto.cs
@@ -9,5 +9,7 @@
         public string Address { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
