a0a02b8db9e9531f2819af91ced2b60972d0ee72
Andrew <andrew.1.kulikov@gmail.com>
2019-11-26 03:39:13 +0300

    Add card number to regisrtation

--- iwor.api/iwor.api/DTOs/RegisterDto.cs
+++ iwor.api/iwor.api/DTOs/RegisterDto.cs
@@ -10,6 +10,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
+        public string CardNumber { get; set; }
         public string ImageUrl { get; set; }
     }
 }
